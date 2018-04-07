9677e5ef95d55c6d2df7eaf10fe6837daade99c5
Chad Rosenquist <chadrosenquist@gmail.com>
2018-04-07 08:11:19 -0500

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
+"""
+To upload to PyPI:
+https://packaging.python.org/tutorials/distributing-packages/
+
+python setup.py sdist bdist_wheel
+twine upload dist/*
+"""
 setup(name='logging-test-case',
       version='1.1',
       description='Provides class LoggingTestCase to help test log files.',
+      keywords='unit testing log files logging regression',
       author='Chad Rosenquist',
-      author_email='ChadRosenquist@hotmail.com',
+      author_email='chadrosenquist@gmail.com',
       url='https://github.com/chadrosenquist/logging-test-case',
       packages=['loggingtestcase'],
-      license='MIT'
+      license='MIT',
+      python_requires='>=3.6'
 )
